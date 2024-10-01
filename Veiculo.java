4d24fa068391e5ba051a08020b005e8f26aabefe
Clarice Christine <142429120+clarisv@users.noreply.github.com>
2024-10-01 15:39:33 -0300

    Veiculo.java

---
+++ Veiculo.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.projetoh;
+
+public class Veiculo {
+   
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+    
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    public void acelerar() {
+        System.out.println("O veículo está acelerando.");
+    }
+    
+    public void exibirInformacoes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+}
+
+    
+
